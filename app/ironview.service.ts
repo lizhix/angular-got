37752b0e4082ec35f8bace7e761c64782fe5489e
GetCodez78 <ziheng.li@gmail.com>
2016-11-29 00:03:06 -0500

    add 2, remove 2

--- app/ironview.service.ts
+++ app/ironview.service.ts
@@ -4,22 +4,19 @@ import { Headers, Http } from '@angular/http';
 @Injectable()
 export class IronViewService {
 
-constructor(private http: Http) { }
+    constructor(private http: Http) { }
 
     private handleError(error: any): Promise<any> {
-    console.error('An error occurred', error); // for demo purposes only
-    return Promise.reject(error.message || error);
-  }
+        console.error('An error occurred', error); // for demo purposes only
+        return Promise.reject(error.message || error);
+    }
 
 
     hammer(): string {
-
-
-
         return "Strike";
     }
 
-  
-    
+
+
 
 }
